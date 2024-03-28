fd828d09d42ab2608c955c74cf656196d3c86e4d
herbet <2660135302@qq.com>
2024-03-28 22:16:41 +0800

    update merge

--- tools/merge.py
+++ tools/merge.py
@@ -1,23 +1,23 @@
 # -*encoding:gbk*-
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfReader, PdfWriter
 
 # 列出你想要拼接的PDF文件
 pdf_files = ['pdf1.pdf','pdf2.pdf']
-pdf_writer = PdfFileWriter()
+pdf_writer = PdfWriter()
 
 for pdf_file in pdf_files:
     # 打开PDF文件
     f = open(pdf_file, 'rb')
-    pdf_reader = PdfFileReader(f)
+    pdf_reader = PdfReader(f)
     # 将每一页添加到PdfFileWriter对象中
     if pdf_file==pdf_files[0]:
         for page_num in range(2):
-            page = pdf_reader.getPage(page_num)
-            pdf_writer.addPage(page)
+            page = pdf_reader.pages[page_num]
+            pdf_writer.add_page(page)
     else:
-        for page_num in range(2,pdf_reader.getNumPages()):
-            page = pdf_reader.getPage(page_num)
-            pdf_writer.addPage(page)
+        for page_num in range(2,len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            pdf_writer.add_page(page)
 
 # 写入到一个新的PDF文件中
 with open('merged.pdf', 'wb') as out:
